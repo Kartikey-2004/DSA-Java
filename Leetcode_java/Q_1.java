bfaca5f2005158083c039622960cd26cb9abe327
Kartikey Gupta <kartikeyg.2004@gmail.com>
2025-07-05 01:23:06 +0530

    modified:   Leetcode_java/Q_1.java 	new file:   Leetcode_java/Q_67.java

--- Leetcode_java/Q_1.java
+++ Leetcode_java/Q_1.java
@@ -1,14 +1,18 @@
 
+import java.util.HashMap;
+
 public class Q_1 {
 
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> num_inx = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[]{i, j};
-                }
+            int req_num = target - nums[i];
+            if (num_inx.containsKey(req_num)) {
+                int[] arr={num_inx.get(req_num),i};
+                return arr; 
             }
+            num_inx.put(nums[i],i);
         }
-        return new int[]{};
+        return null;
     }
 }
